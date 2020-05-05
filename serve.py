1d7e139afc5326007116a65f8f740caa2d30692c
Alexander Jacob <alexander.jacob@jacob-consulting.de>
2020-05-05 11:14:22 +0200

    add simple http server

---
+++ serve.py
@@ -0,0 +1,11 @@
+import http.server
+import socketserver
+
+PORT = 8888
+Handler = http.server.SimpleHTTPRequestHandler
+
+if __name__ == '__main__':
+
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
